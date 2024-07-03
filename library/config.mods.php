b4a7bba2095c6adb2a46cb539614e611872166d5
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-07-03 20:13:57 +0700

    Update config.mods.php

--- library/config.mods.php
+++ library/config.mods.php
@@ -6,7 +6,7 @@
 if (!defined('BB_ROOT')) die(basename(__FILE__));
 
 // Режим: Новый год
-$bb_cfg['new_year_mode'] = true;
+$bb_cfg['new_year_mode'] = ((date('n') == 12 and date('j') > 21) or (date('n') == 1 and date('j') < 10));
 
 // Дата запуска форума в статистике на главной
 $bb_cfg['show_board_start_date'] = true;
