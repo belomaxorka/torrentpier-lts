7edbee1ad35073ff72a71e3096eb1fbc3d5167d1
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-07-11 00:50:40 +0700

    Create thanks.php

---
+++ library/ajax/thanks.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * TorrentPier – Bull-powered BitTorrent tracker engine
+ *
+ * @copyright Copyright (c) 2005-2024 TorrentPier (https://torrentpier.com)
+ * @link      https://github.com/torrentpier/torrentpier for the canonical source repository
+ * @license   https://github.com/torrentpier/torrentpier/blob/master/LICENSE MIT License
+ */
+
+if (!defined('IN_AJAX')) {
+	die(basename(__FILE__));
+}
+
+global $bb_cfg, $lang, $userdata;
+
+if (!$bb_cfg['tor_thank']) {
+	$this->ajax_die($lang['MODULE_OFF']);
+}
+
+if (!$mode = (string)$this->request['mode']) {
+	$this->ajax_die('invalid mode (empty)');
+}
+
+if (!$topic_id = (int)$this->request['topic_id']) {
+	$this->ajax_die($lang['INVALID_TOPIC_ID']);
+}
+
+if (!$to_user_id = (int)$this->request['to_user_id']) {
+	$this->ajax_die('invalid to_user_id (empty)');
+}
+
+switch ($mode) {
+	case 'add':
+		if (IS_GUEST) {
+			$this->ajax_die($lang['NEED_TO_LOGIN_FIRST']);
+		}
+
+		if (DB()->fetch_row('SELECT poster_id FROM ' . BB_BT_TORRENTS . " WHERE topic_id = $topic_id AND poster_id = " . $userdata['user_id'])) {
+			$this->ajax_die($lang['LIKE_OWN_POST']);
+		}
+
+		if (DB()->fetch_row('SELECT topic_id FROM ' . BB_THX . " WHERE topic_id = $topic_id  AND user_id = " . $userdata['user_id'])) {
+			$this->ajax_die($lang['LIKE_ALREADY']);
+		}
+
+		$columns = 'topic_id, user_id, to_user_id, time';
+		$values = "$topic_id, {$userdata['user_id']}, $to_user_id, " . TIMENOW;
+		DB()->query('INSERT IGNORE INTO ' . BB_THX . " ($columns) VALUES ($values)");
+		break;
+	case 'get':
+		if (IS_GUEST && !$bb_cfg['tor_thanks_list_guests']) {
+			$this->ajax_die($lang['NEED_TO_LOGIN_FIRST']);
+		}
+
+		$sql = DB()->fetch_rowset('SELECT u.username, u.user_rank, u.user_id, thx.* FROM ' . BB_THX . ' thx, ' . BB_USERS . " u WHERE thx.topic_id = $topic_id AND thx.user_id = u.user_id");
+
+		$user_list = array();
+		foreach ($sql as $row) {
+			$user_list[] = '<b>' . profile_url($row) . ' <i>(' . bb_date($row['time']) . ')</i></b>';
+		}
+
+		$this->response['html'] = join(', ', $user_list) ?: $lang['NO_LIKES'];
+		break;
+	default:
+		$this->ajax_die('Invalid mode: ' . $mode);
+}
+
+$this->response['mode'] = $mode;
