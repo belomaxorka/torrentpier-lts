2a6b98baea4287ac7c117679d1c3a72697ff31e0
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-07-11 12:53:20 +0700

    Update thanks.php

--- library/ajax/thanks.php
+++ library/ajax/thanks.php
@@ -30,7 +30,7 @@ if (!$topic_id = (int)$this->request['topic_id']) {
 	$this->ajax_die($lang['INVALID_TOPIC_ID']);
 }
 
-if (!$to_user_id = (int)$this->request['to_user_id']) {
+if (!$to_user_id = (int)$this->request['to_user_id']) { // poster_id
 	$this->ajax_die('invalid to_user_id (empty)');
 }
 
@@ -47,34 +47,48 @@ switch ($mode) {
 		}
 
 		// Проверка на повторное спасибо
-		if (DB()->fetch_row('SELECT topic_id FROM ' . BB_THX . " WHERE topic_id = $topic_id  AND user_id = " . $userdata['user_id'])) {
+		if (DB()->fetch_row("SELECT topic_id FROM " . BB_THX . " WHERE topic_id = $topic_id  AND user_id = " . $userdata['user_id'])) {
 			$this->ajax_die($lang['LIKE_ALREADY']);
 		}
 
+		// Добавление спасибо
 		$columns = 'topic_id, user_id, to_user_id, time';
 		$values = "$topic_id, {$userdata['user_id']}, $to_user_id, " . TIMENOW;
-		DB()->query('INSERT IGNORE INTO ' . BB_THX . " ($columns) VALUES ($values)");
+		DB()->query("INSERT IGNORE INTO " . BB_THX . " ($columns) VALUES ($values)");
+
+		// Проверка на лимит
+		DB()->query("
+			DELETE FROM " . BB_THX . "
+			WHERE user_id IN (
+				SELECT user_id
+				FROM " . BB_THX . "
+				WHERE topic_id = $topic_id
+				ORDER BY time
+				LIMIT 1
+			)
+			AND (SELECT COUNT(*) FROM " . BB_THX . " WHERE topic_id = $topic_id) > $max_users;
+		");
 		break;
 	case 'get':
-		$max_users = 50;
+		// Проверка на возможность просмотра списка
 		if (IS_GUEST && !$bb_cfg['tor_thanks_list_guests']) {
 			$this->ajax_die($lang['NEED_TO_LOGIN_FIRST']);
 		}
 
-		$sql = DB()->fetch_rowset('SELECT u.username, u.user_rank, u.user_id, thx.* FROM ' . BB_THX . ' thx, ' . BB_USERS . " u WHERE thx.topic_id = $topic_id AND thx.user_id = u.user_id");
-
 		$user_list = array();
+		$sql = DB()->fetch_rowset("
+				SELECT u.username, u.user_rank, u.user_id, thx.*
+				FROM " . BB_THX . " thx, " . BB_USERS . " u
+				WHERE thx.topic_id = $topic_id
+					AND thx.user_id = u.user_id
+		");
 		foreach ($sql as $row) {
-			$user_list[] = (count($user_list) >= $max_users) ? $row['user_id'] : ('<b>' . profile_url($row) . ' <i>(' . bb_date($row['time']) . ')</i></b>');
+			$user_list[] = '<b>' . profile_url($row) . ' <i>(' . bb_date($row['time']) . ')</i></b>';
 		}
 
 		if (!empty($user_list)) {
 			$this->response['count_likes'] = "&nbsp;(" . declension(count($user_list), 'times') . ")";
-			$html = implode(", ", array_slice($user_list, 0, $max_users));
-			if (count($user_list) > $max_users) {
-				$html .= ', ...';
-			}
-			$this->response['html'] = $html;
+			$this->response['html'] = implode(", ", $user_list);
 		} else {
 			$this->response['html'] = $lang['NO_LIKES'];
 		}
