574adc188e888067af4f3a555f8b67023ca70a4a
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-07-20 14:12:40 +0700

    Update book.php

--- book.php
+++ book.php
@@ -15,7 +15,7 @@ $page_cfg['use_tablesorter'] = true;
 // Init userdata
 $user->session_start(array('req_login' => true));
 
-$sql = DB()->fetch_rowset("SELECT b.*, t.*, f.* FROM " . BB_BOOK . " b
+$sql = DB()->fetch_rowset("SELECT t.*, f.forum_id, f.forum_name FROM " . BB_BOOK . " b
 								INNER JOIN " . BB_TOPICS . " t ON(t.topic_id = b.topic_id)
 								INNER JOIN " . BB_FORUMS . " f ON(f.forum_id = b.forum_id)
 							WHERE user_id = {$userdata['user_id']}");
